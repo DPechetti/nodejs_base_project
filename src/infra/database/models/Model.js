9d4577fa5bf29c219cb202b1721d028d00c2e9a7
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-03 06:53:43 -0300

    default model created

---
+++ src/infra/database/models/Model.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { OperationException } = require('../../exception');
+
+module.exports = class Model {
+  constructor({ providerConnection, logger, collectionName, schema, indexes = [] }) {
+    this.logger = logger;
+    this.collectionName = collectionName;
+    this.indexes = indexes;
+
+    this.validateParams();
+
+    const connection = providerConnection.connection;
+
+    indexes.forEach(index => {
+      const { fields, unique } = index;
+
+      schema.index(fields, unique);
+    });
+
+    return connection.model(collectionName, schema);
+  }
+
+  validateParams() {
+    if (!this.collectionName) {
+      const errorMessage = 'Missing collection name';
+      this.logger.error(errorMessage);
+      throw new OperationException(errorMessage);
+    }
+
+    if (this.indexes.length === 0) {
+      const errorMessage = 'Missing index';
+      this.logger.error(errorMessage);
+      throw new OperationException(errorMessage);
+    }
+  }
+};
