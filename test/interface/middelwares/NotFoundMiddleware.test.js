9b41bf24adff44c0c7677c7f0b916c9711c0fc63
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-15 07:38:38 -0300

    not found middleware test created

---
+++ test/interface/middelwares/NotFoundMiddleware.test.js
@@ -0,0 +1,15 @@
+const NotFoundMiddleware = require('../../../src/interface/middlewares/NotFoundMiddleware')();
+const { NotFoundException } = require('../../../src/infra/exception');
+jest.mock('../../../src/infra/exception/NotFoundException');
+
+describe('Error Middleware', () => {
+
+  test('Should return a serialized error', () => {
+    try {
+      NotFoundMiddleware();
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+      expect(NotFoundException).toHaveBeenCalledTimes(1);
+    }
+  });
+});
