008907c6f3925a68ae76ac5df2ec8a949aaf1e5f
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-16 21:47:33 -0300

    add not found validation in get batatinha service

--- test/app/services/GetBatatinhaService.test.js
+++ test/app/services/GetBatatinhaService.test.js
@@ -16,4 +16,34 @@ describe('GetBatatinhaService', () => {
 
     expect(foundBatatinha).toEqual(batatinha);
   });
+
+  test('Should find batatinha, call batatinha domain, call batatinha repository and return batatinha', async () => {
+    const batatinha = generateBatatinhaRequest();
+    const { batatinha_header, batatinha_id } = batatinha;
+    const batatinhaRepository = {
+      get: () => Promise.resolve(null)
+    };
+
+    const getBatatinhaService = GetBatatinhaService({ batatinhaRepository });
+    try {
+      await getBatatinhaService.execute({ batatinha_header, batatinha_id });
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+
+      expect(error).toHaveProperty('code');
+      expect(error.code).toStrictEqual('404');
+
+      expect(error).toHaveProperty('message');
+      expect(error.message).toStrictEqual('Not Found');
+
+      expect(error).toHaveProperty('details');
+      expect(error.details).toHaveLength(1);
+
+      expect(error.details[0]).toHaveProperty('error_code');
+      expect(error.details[0].error_code).toStrictEqual('Batatinha not found');
+
+      expect(error.details[0]).toHaveProperty('error_message');
+      expect(error.details[0].error_message).toStrictEqual('Batatinha was not found with header informed');
+    }
+  });
 });
